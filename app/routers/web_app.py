59d9d54f8f01d4159073ef141901aac93796284c
creator <ctarsoliya@gmail.com>
2023-04-30 16:45:54 +0530

    ui added and some bug fix

--- app/routers/web_app.py
+++ app/routers/web_app.py
@@ -1,16 +1,14 @@
 import os
 
 from fastapi import APIRouter, Request
+from starlette.responses import RedirectResponse
+
 from app.dependencies import templates
 
 main_router = APIRouter(tags=['web app'])
 
 
 @main_router.get('/')
-async def get(request: Request):
-    print("ENV: ", os.getenv('WebSocketURL', 'ws://localhost:8000/ws'))
-    return templates.TemplateResponse("main_page.html",
-                                      {"request": request,
-                                       "WebSocketURL": os.getenv('WebSocketURL', 'ws://localhost:8000/ws')
-                                       }
-                                      )
+async def get():
+    response = RedirectResponse(url='/app')
+    return response
